9d0509a277934043a6d5683e66958901d09b3450
sandatjepil <masamik2222@gmail.com>
2024-04-23 17:13:02 +0700

    ggg

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ if ! [ -d "$KERNELDIR/clang" ]; then
   echo "Clang not found! Cloning..."
   if [ $COMP = "trb" ]; then
     git clone https://gitlab.com/varunhardgamer/trb_clang --depth=1 -b 17 --single-branch clang || (echo "Cloning failed! Aborting..."; exit 1)
-  if [ $COMP = "proton" ]; then
+  elif [ $COMP = "proton" ]; then
     git clone https://gitlab.com/LeCmnGend/clang --depth=1 -b clang-14 --single-branch clang || (echo "Cloning failed! Aborting..."; exit 1)
   elif [ $COMP = "ew" ]; then
     git clone https://gitlab.com/Tiktodz/electrowizard-clang.git --depth=1 -b 16 --single-branch clang || (echo "Cloning failed! Aborting..."; exit 1)
