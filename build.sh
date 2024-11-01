db0e1eb283058710440f9b721a39859df00b7b0e
Purrr <masamik2222@gmail.com>
2024-11-01 15:25:41 +0700

    kk

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ VARIANT="CLO"
 
 # set compiler
 # "neutron" || "trb" || "ew" || "proton"
-COMP="proton"
+COMP="ew"
 
 sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=".lnx.4.4.r42-rel"/g' arch/arm64/configs/X00TD_defconfig
 
