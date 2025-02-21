e4a29a6d15e26db0cfe096a59aa66f346128fca4
Agus Suristyono <sandatjepil@proton.me>
2025-02-21 07:07:42 +0700

    ff
    Signed-off-by: Agus Suristyono <sandatjepil@proton.me>

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-export TZ="Asia/Jakarta"
-
-if [ -f kernel/arch/arm64/configs/X00TD_defconfig ]; then
-    cd kernel
-    KERNELDIR=$(pwd)
-else
-    echo "Kernel Cloning Failed! aborting..."
-    exit 1
-fi
+############################################################
+[[ -f kernel/Makefile ]] || exit 1
+cd kernel; export KERNELDIR=$(pwd) TZ="Asia/Jakarta"
+blue='\033[0;34m'; red='\033[0;31m'; nocol='\033[0m'
+log(){
+	case $1 in
+		info) echo -e "$blue$2$nocol";;
+		warn) echo -e "$red$2$nocol";;
+	esac
+}
+############################################################
 
 # Additional command (if you're lazy to commit :v)
 sed -i 's/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-Heliasts-Ἡλιαστής🏛"/g' arch/arm64/configs/X00TD_defconfig
 
-#set -e
 # Set the Variables
 KERNELNAME="Heliasts"
 DEVICENAME="Asus Zenfone Max Pro M1 (X00TD)"
@@ -29,51 +30,58 @@ VARIANT="End Of Life"
 COMP=1
 
 # Build with KSU?
-# y = yes || n = no
+# 1 = true || 2 = false
 # b = build both KSU & Non-KSU
 WITHKSU=b
 
 # Sign the build?
-# 1 = yes || 0 = no
+# 1 = true || 2 = false
 SIGN=1
 
-# Define is the target telegram is a supergroup or not
+############################################################
+# Push to Telegram?
 # 1 = true || 0 = false
+PUSHTG=1
+# Target telegram is a supergroup?
 TG_SUPER=1
 
-# Additional Variables
-KERNEL_DEFCONFIG=X00TD_defconfig
-DATE=$(date '+%d %m %Y')
-export KBUILD_BUILD_TIMESTAMP=$(date)
-export KBUILD_BUILD_USER="Purrr"
-export KBUILD_BUILD_HOST="WizardPrjkt"
-export ARCH=arm64
-export SUBARCH=arm64
-
-############################################################
 tg_post_msg(){
-if [[ $TG_SUPER == 1 ]]; then
-    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
-    -d "message_thread_id=$TG_TOPIC_ID" -d "parse_mode=html" -d "text=$1" \
-    -d "chat_id=$TG_CHAT_ID" -d "disable_web_page_preview=true"
+if [[ $PUSHTG == 1 ]]; then
+	if [[ $TG_SUPER == 1 ]]; then
+	    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+	    -d "message_thread_id=$TG_TOPIC_ID" -d "parse_mode=html" -d "text=$1" \
+	    -d "chat_id=$TG_CHAT_ID" -d "disable_web_page_preview=true"
+	else
+		curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+		-d "chat_id=$TG_CHAT_ID" -d "disable_web_page_preview=true" \
+		-d "parse_mode=html" -d "text=$1"
+	fi
 else
-	curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
-	-d "chat_id=$TG_CHAT_ID" -d "disable_web_page_preview=true" \
-	-d "parse_mode=html" -d "text=$1"
+	log info "$1"
 fi
 }
 tg_post_build() {
-if [[ $TG_SUPER == 1 ]]; then
-	curl -s "https://api.telegram.org/bot$TG_TOKEN/sendDocument" -F "document=@$1" \
-	-F "chat_id=$TG_CHAT_ID" -F "disable_web_page_preview=true" \
-	-F "parse_mode=Markdown" -F "caption=$2" -F "message_thread_id=$TG_TOPIC_ID"
+if [[ $PUSHTG == 1 ]]; then
+	if [[ $TG_SUPER == 1 ]]; then
+		curl -s "https://api.telegram.org/bot$TG_TOKEN/sendDocument" -F "document=@$1" \
+		-F "chat_id=$TG_CHAT_ID" -F "disable_web_page_preview=true" \
+		-F "parse_mode=Markdown" -F "caption=$2" -F "message_thread_id=$TG_TOPIC_ID"
+	else
+		curl -s "https://api.telegram.org/bot$TG_TOKEN/sendDocument" -F "document=@$1" \
+		-F "chat_id=$TG_CHAT_ID" -F "disable_web_page_preview=true" \
+		-F "parse_mode=Markdown" -F "caption=$2"
+	fi
 else
-	curl -s "https://api.telegram.org/bot$TG_TOKEN/sendDocument" -F "document=@$1" \
-	-F "chat_id=$TG_CHAT_ID" -F "disable_web_page_preview=true" \
-	-F "parse_mode=Markdown" -F "caption=$2"
+	log info "$2"
 fi
 }
 ############################################################
+
+# Additional Variables
+KERNEL_DEFCONFIG=X00TD_defconfig
+DATE=$(date '+%d %m %Y') ZIPDATE=$(date '+%y%m%d%H%M')
+export KBUILD_BUILD_TIMESTAMP=$(date) KBUILD_BUILD_USER="Purrr" \
+KBUILD_BUILD_HOST="WizardPrjkt" ARCH=arm64 SUBARCH=arm64
 
 tg_post_msg "🕒 <b>`date '+%d %b %Y, %H:%M %Z'`</b>
 Masterpiece creation starts! 
@@ -81,7 +89,7 @@ Version <b>$KERVER</b> for <b>$DEVICENAME</b>.
 Crafted with <b>$(source /etc/os-release && echo "$NAME" | cut -d" " -f1)</b>.
 Compilation progress <a href='$CIRCLE_BUILD_URL'>click here!</a>."
 
-echo "Cloning Clang"
+log info "****Cloning Clang****"
 case $COMP in
 	1)
 		mkdir -p clang && cd clang
@@ -91,29 +99,29 @@ case $COMP in
 		[[ -f "$KERNELDIR/clang/bin/clang" ]] || exit 1
 		;;
 	2)
-		git clone https://gitlab.com/varunhardgamer/trb_clang --depth=1 -b 17 --single-branch clang || exit 1
+		git clone https://gitlab.com/varunhardgamer/trb_clang --depth=1 -qb 17 --single-branch clang || exit 1
 		export PATH="$KERNELDIR/clang/bin:$PATH"
 		;;
 	3)
 		# git clone https://gitlab.com/Tiktodz/electrowizard-clang.git --depth=1 -b 16 --single-branch clang || exit 1
-		mkdir "$KERNELDIR/clang" && cd "$KERNELDIR/clang"
-		wget -O ew.tar.gz https://github.com/Tiktodz/electrowizard-clang/releases/download/ElectroWizard-Clang-18.1.8-release/ElectroWizard-Clang-18.1.8.tar.gz && tar -xzf ew.tar.gz && rm -f ew.tar.gz && cd $KERNELDIR
+		mkdir -p "$KERNELDIR/clang" && cd "$KERNELDIR/clang"
+		wget -qO ew.tar.gz https://github.com/Tiktodz/electrowizard-clang/releases/download/ElectroWizard-Clang-18.1.8-release/ElectroWizard-Clang-18.1.8.tar.gz && tar -xzf ew.tar.gz && rm -f ew.tar.gz && cd $KERNELDIR
 		export PATH="$KERNELDIR/clang/bin:$PATH"
 		[[ -f "$KERNELDIR/clang/bin/clang" ]] || exit 1
 		;;
 	4)
-		git clone https://gitlab.com/LeCmnGend/clang --depth=1 -b clang-13 --single-branch clang || exit 1
+		git clone https://gitlab.com/LeCmnGend/clang --depth=1 -qb clang-13 --single-branch clang || exit 1
 		export PATH="$KERNELDIR/clang/bin:$PATH"
 		;;
 	5)
 		apt-get install wget libncurses5 -y
-		wget -O sdc.tar.gz https://github.com/sandatjepil/SDClang/releases/download/v14.1.5/sdclangxgcc.tar.gz && tar -xzf sdc.tar.gz && rm -f sdc.tar.gz
+		wget -qO sdc.tar.gz https://github.com/sandatjepil/SDClang/releases/download/v14.1.5/sdclangxgcc.tar.gz && tar -xzf sdc.tar.gz && rm -f sdc.tar.gz
 		export PATH="$KERNELDIR/sdclang/bin:$KERNELDIR/gcc64/bin:$KERNELDIR/gcc32/bin:$PATH"
 		export LD_LIBRARY_PATH="$KERNELDIR/sdclang/lib:$LD_LIBRARY_PATH"
 		[[ -f "$KERNELDIR/sdclang/bin/clang" ]] || exit 1
 		;;
 	*)
-		echo "Clang unavailable! Aborting..."
+		tg_post_msg "Clang unavailable! Aborting..."
 		exit 1
 		;;
 esac
@@ -124,17 +132,16 @@ else
 	export KBUILD_COMPILER_STRING=$("$KERNELDIR"/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 fi
 
-# Generating Changelog
+log info "****Generating Changelog****"
 echo "<b><#selectbg_g>$(date)</#></b>" > changelog
 git log --oneline -n15 | cut -d " " -f 2- | awk '{print "<*> " $(A) "</*>"}' >> changelog
 echo "" >> changelog
 echo "<b><#selectbg_g>Aroma Installer config by: @ItsRyuujiX</#></b>" >> changelog
 
-echo "**** AnyKernel3 Time ****"
+log info "**** AnyKernel3 Time ****"
 AK3DIR=$KERNELDIR/AnyKernel3
-echo "Cloning AnyKernel3"
-if ! git clone --depth=1 -b zeus https://github.com/sandatjepil/AnyKernel3 AnyKernel3; then
-	echo "Cloning failed! Aborting..."
+if ! git clone -qb zeus --depth=1 https://github.com/sandatjepil/AnyKernel3 AnyKernel3; then
+	log warn "Cloning failed! Aborting..."
 	tg_post_msg "Cloning AnyKernel3 Failed, aborting compilation"
 	exit 1
 fi
@@ -167,14 +174,14 @@ sed -i "s/KDEVICE/Zenfone Max Pro M1/g" aroma-config
 sed -i "s/KBDATE/$DATE/g" aroma-config
 sed -i "s/KVARIANT/$VARIANT/g" aroma-config
 cd "$KERNELDIR"
-echo "***** AnyKernel3 Done! *****"
+log info "***** AnyKernel3 Done! *****"
 
 # Speed up build process
 MAKE="./makeparallel"
 
 # Now building process is a function
 start_cooking() {
-	FINAL_ZIP="$KERNELNAME-$1-$KERVER-$(date '+%y%m%d%H%M')"
+	FINAL_ZIP="$KERNELNAME-$1-$KERVER-$ZIPDATE"
 	
 	case $1 in
 		KSU)
@@ -196,28 +203,15 @@ start_cooking() {
 			;;
 	esac
 
-	
 	# Clean Up Output Directory
-	if ! [[ -d "$KERNELDIR"/out ]]; then
-		mkdir -p "$KERNELDIR"/out
-	else
-		rm -rf " $KERNELDIR"/out
-		mkdir -p "$KERNELDIR"/out
-	fi
+	[[ -d "$KERNELDIR"/out ]] && rm -rf "$KERNELDIR"/out
 	
 	BUILD_START=$(date +"%s")
-	blue='\033[0;34m'
-	cyan='\033[0;36m'
-	yellow='\033[0;33m'
-	red='\033[0;31m'
-	nocol='\033[0m'
-
-	echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
-	echo -e "$blue***********************************************"
-	echo "          BUILDING KERNEL          "
-	echo -e "***********************************************$nocol"
+	log info "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+	log info "***********************************************"
+	log info "          BUILDING KERNEL          "
+	log info "***********************************************"
 	make $KERNEL_DEFCONFIG O=out 2>&1 | tee -a error.log
-
 	case $COMP in
 		4)
 			make -j$(nproc --all) O=out LLVM=1 \
@@ -263,19 +257,17 @@ start_cooking() {
 	BUILD_END=$(date +"%s")
 	DIFF=$(($BUILD_END - $BUILD_START))
 	
-	echo "**** Kernel Compilation Completed ****"
-	echo "**** Verify Image.gz-dtb ****"
-	
 	if ! [[ -f $KERNELDIR/out/arch/arm64/boot/Image.gz-dtb ]];then
-	    tg_post_build "error.log" "Compile Error!!"
-	    echo "$red Compile Failed!!!$nocol"
+	    tg_post_build "error.log" "Compile failed!!"
+	    log warn "**** Compile Failed!!! ****"
 	    exit 1
 	fi
-
-	echo "**** Copying Image.gz-dtb ****"
+	log info "**** Kernel build completed ****"
+	
+	log info "**** Copying Image.gz-dtb ****"
 	cp -af $KERNELDIR/out/arch/arm64/boot/Image.gz-dtb $AK3DIR
 	
-	echo "**** Time to zip up! ****"
+	log info "**** Time to zip up! ****"
 	cd $AK3DIR
 	zip -r9 ../$FINAL_ZIP.zip * -x .git README.md anykernel-real.sh .gitignore zipsigner* *.zip
 	cd $KERNELDIR
@@ -297,7 +289,7 @@ start_cooking() {
 	
 	MD5CHECK=$(md5sum "$FINAL_ZIP.zip" | cut -d' ' -f1)
 
-	echo "**** Uploading your zip now ****"
+	log info "**** Uploading your zip now ****"
 	tg_post_build "$FINAL_ZIP.zip" "⏳ *Compile Time*
  $(($DIFF / 60)) minute(s) $(($DIFF % 60)) seconds
 📱 *Device*
@@ -311,7 +303,7 @@ ${KBUILD_COMPILER_STRING}
 💾 *MD5 Checksum*
 \`${MD5CHECK}\`
 🆕 *Last Changelog*
-\``git log --oneline -n1 | cut -d" " -f2-`\`
+`git log --oneline -n1 | cut -d" " -f2-`
 
 ⚠️ ${BONUS_MSG}"
 
@@ -320,10 +312,10 @@ ${KBUILD_COMPILER_STRING}
 }
 
 case $WITHKSU in
-	y)
+	1)
 		start_cooking "KSU"
 		;;
-	n)
+	2)
 		start_cooking "NoKSU"
 		;;
 	b)
